27c996997cac8e1d7e08363f213a9bb571163e63
Dheeraj Sharma <72129959+dheerajsharma0401@users.noreply.github.com>
2021-08-17 19:18:45 +0530

    Update euclid.m

--- euclid.m
+++ euclid.m
@@ -1,8 +1,8 @@
 function dist = euclid(v1, v2)
 % %=======================================================================
 % This function computes Eucledian Distance between vector v1 and v2
-% Author: Arwan Ahmad Khoiruddin
-% Date modified: 30 March 2016
+% Author: Dheeraj Sharma
+% Date modified: 20 December 2020
 % %=======================================================================
 
 V = v1-v2;
